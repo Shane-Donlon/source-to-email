45dec840d1551e8f9b1f98849e53ac072161eaaf
Shane-Donlon <shane.donlon@live.com>
2024-03-26 19:04:53 +0000

    Script update: email template title and language add

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -91,15 +91,17 @@ function working() {
 
   let bgColor = document.querySelector("#body-background-color").value;
   let color = document.querySelector("#font-color").value;
+  let title = textAreaDocument.querySelectorAll("h1");
   html = `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>${title[0].textContent}</title>
 </head>
 <body style="width: 100%;margin:0;padding:0; background-color:${bgColor}; color:${color}" >
-<table class="body" style="background-color:${bgColor}; color:${color};width: 100%;margin:0;padding:0;"> <tr> <td class="center" align="center" valign="top"> <center> <table class="container" style="width:80%;max-width:600px; margin-left:auto; margin-right:auto;" > <tr> <td class="email-content"></td> </tr> </table> </center> </td> </tr> </table>
+<table role="presentation" class="body" style="background-color:${bgColor}; color:${color};width: 100%;margin:0;padding:0;"> <tr> <td class="center" align="center" valign="top"> <center> <table role="presentation"  class="container" style="width:80%;max-width:600px; margin-left:auto; margin-right:auto;" > <tr> <td class="email-content"></td> </tr> </table> </center> </td> </tr> </table>
 </body>
   </html>`;
 
