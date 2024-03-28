39ec9358a3814c2b6611f6565f016ad5d44d69b8
Shane-Donlon <shane.donlon@live.com>
2024-03-28 17:07:09 +0000

    Script update: HTML Email template update

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,4 +1,5 @@
-let btn = document.querySelector(".convert");
+let btn = document.querySelector(".btn-convert");
+let resetLocalStorageBtn = document.querySelector(".btn-reset-local-storage");
 let outputCode = document.querySelector("#output-code");
 
 let inputs = document.querySelectorAll("label + *");
@@ -108,28 +109,406 @@ function working() {
 
   let title = textAreaDocument.querySelectorAll("h1");
 
-  html = `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" lang="en" margin:0;padding:0; background-color:${bgColor}; color:${color}>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>${title[0].textContent}</title>
-</head>
-<body style="margin:0;padding:0; background-color:${bgColor}; color:${color}" >
-<table role="presentation" class="body" style="background-color:${bgColor}; color:${color};width: 100%;margin:0;padding:0;"> <tr> <td class="center" align="center" valign="top"> <center> <table role="presentation"  class="container" style="width:80%;max-width:600px; margin-left:auto; margin-right:auto;" > <tr> <td class="email-content"></td> </tr> </table> </center> </td> </tr> </table>
-</body>
-  </html>`;
+  //   html = `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+  // <html xmlns="http://www.w3.org/1999/xhtml" lang="en" margin:0;padding:0; background-color:${bgColor}; color:${color}>
+  // <head>
+  // <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  // <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  // <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  // <title>${title[0].textContent}</title>
+  // </head>
+  // <body style="margin:0;padding:0; background-color:${bgColor}; color:${color}" >
+  // <table role="presentation" class="body" style="background-color:${bgColor}; color:${color};width: 100%;margin:0;padding:0;"> <tr> <td class="center" align="center" valign="top"> <center> <table role="presentation"  class="container" style="width:80%;max-width:600px; margin-left:auto; margin-right:auto;" > <tr> <td class="email-content"></td> </tr> </table> </center> </td> </tr> </table>
+  // </body>
+  //   </html>`;
 
+  html = `<!DOCTYPE html>
+  <html
+    lang="en"
+    xmlns="https://www.w3.org/1999/xhtml"
+    xmlns:o="urn:schemas-microsoft-com:office:office"
+  >
+    <head>
+      <meta charset="utf-8" />
+      <meta name="viewport" content="width=device-width,initial-scale=1" />
+      <meta name="x-apple-disable-message-reformatting" />
+      <!--[if !mso]><!-->
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <!--<![endif]-->
+      <title></title>
+  
+      <style media="all" type="text/css">
+        /* -------------------------------------
+        GLOBAL RESETS
+    ------------------------------------- */
+  
+        body {
+          font-family: Helvetica, sans-serif;
+          -webkit-font-smoothing: antialiased;
+          font-size: 16px;
+          line-height: 1.3;
+          -ms-text-size-adjust: 100%;
+          -webkit-text-size-adjust: 100%;
+        }
+  
+        table {
+          border-collapse: separate;
+          mso-table-lspace: 0pt;
+          mso-table-rspace: 0pt;
+          width: 100%;
+        }
+  
+        table td {
+          font-family: Helvetica, sans-serif;
+          font-size: 16px;
+          vertical-align: top;
+        }
+        /* -------------------------------------
+        BODY & CONTAINER
+    ------------------------------------- */
+  
+        body {
+          background-color: :${bgColor};
+          color: ${color};
+          margin: 0;
+          padding: 0;
+        }
+  
+        .body {
+          background-color: :${bgColor};
+          color: ${color};
+          width: 100%;
+        }
+  
+        .container {
+          margin: 0 auto !important;
+          max-width: 600px;
+          padding: 0;
+          padding-top: 24px;
+          width: 600px;
+        }
+  
+        .content {
+          box-sizing: border-box;
+          display: block;
+          margin: 0 auto;
+          max-width: 600px;
+          padding: 0;
+        }
+        /* -------------------------------------
+        HEADER, FOOTER, MAIN
+    ------------------------------------- */
+  
+        
+  
+        .wrapper {
+          box-sizing: border-box;
+          padding: 24px;
+        }
+  
+        .footer {
+          clear: both;
+          padding-top: 24px;
+          text-align: center;
+          width: 100%;
+        }
+  
+        .footer td,
+        .footer p,
+        .footer span,
+        .footer a {
+         
+          
+          text-align: center;
+        }
+        /* -------------------------------------
+        TYPOGRAPHY
+    ------------------------------------- */
+  
+        p {
+          font-family: Helvetica, sans-serif;
+          font-size: 16px;
+          font-weight: normal;
+          margin: 0;
+          margin-bottom: 16px;
+        }
+  
+        a {
+         
+          text-decoration: underline;
+        }
+        /* -------------------------------------
+        BUTTONS
+    ------------------------------------- */
+  
+        .btn {
+          box-sizing: border-box;
+          min-width: 100% !important;
+          width: 100%;
+        }
+  
+        .btn > tbody > tr > td {
+          padding-bottom: 16px;
+        }
+  
+        .btn table {
+          width: auto;
+        }
+  
+        .btn table td {
+          
+          border-radius: 4px;
+          text-align: center;
+        }
+  
+        .btn a {
+          
+         
+          border-radius: 4px;
+          box-sizing: border-box;
+         
+          cursor: pointer;
+          display: inline-block;
+          font-size: 16px;
+          font-weight: bold;
+          margin: 0;
+          padding: 12px 24px;
+          text-decoration: none;
+          text-transform: capitalize;
+        }
+  
+        .btn-primary table td {
+          
+        }
+  
+       
+  
+       
+        /* -------------------------------------
+        OTHER STYLES THAT MIGHT BE USEFUL
+    ------------------------------------- */
+  
+        .last {
+          margin-bottom: 0;
+        }
+  
+        .first {
+          margin-top: 0;
+        }
+  
+        .align-center {
+          text-align: center;
+        }
+  
+        .align-right {
+          text-align: right;
+        }
+  
+        .align-left {
+          text-align: left;
+        }
+  
+        .text-link {
+     
+        }
+  
+        .clear {
+          clear: both;
+        }
+  
+        .mt0 {
+          margin-top: 0;
+        }
+  
+        .mb0 {
+          margin-bottom: 0;
+        }
+  
+        .preheader {
+          color: transparent;
+          display: none;
+          height: 0;
+          max-height: 0;
+          max-width: 0;
+          opacity: 0;
+          overflow: hidden;
+          mso-hide: all;
+          visibility: hidden;
+          width: 0;
+        }
+  
+        /* -------------------------------------
+        RESPONSIVE AND MOBILE FRIENDLY STYLES
+    ------------------------------------- */
+  
+        @media only screen and (max-width: 640px) {
+          .main p,
+          .main td,
+          .main span {
+            font-size: 16px !important;
+          }
+          .wrapper {
+            padding: 8px !important;
+          }
+          .content {
+            padding: 0 !important;
+          }
+          .container {
+            padding: 0 !important;
+            padding-top: 8px !important;
+            width: 100% !important;
+          }
+          .main {
+            border-left-width: 0 !important;
+            border-radius: 0 !important;
+            border-right-width: 0 !important;
+          }
+          .btn table {
+            max-width: 100% !important;
+            width: 100% !important;
+          }
+          .btn a {
+            font-size: 16px !important;
+            max-width: 100% !important;
+            width: 100% !important;
+          }
+        }
+        /* -------------------------------------
+        PRESERVE THESE STYLES IN THE HEAD
+    ------------------------------------- */
+  
+        @media all {
+          .ExternalClass {
+            width: 100%;
+          }
+          .ExternalClass,
+          .ExternalClass p,
+          .ExternalClass span,
+          .ExternalClass font,
+          .ExternalClass td,
+          .ExternalClass div {
+            line-height: 100%;
+          }
+          .apple-link a {
+            color: inherit !important;
+            font-family: inherit !important;
+            font-size: inherit !important;
+            font-weight: inherit !important;
+            line-height: inherit !important;
+            text-decoration: none !important;
+          }
+          #MessageViewBody a {
+            color: inherit;
+            text-decoration: none;
+            font-size: inherit;
+            font-family: inherit;
+            font-weight: inherit;
+            line-height: inherit;
+          }
+        }
+      </style>
+  
+      <!--[if mso]>
+        <style type="text/css">
+          table {
+            border-collapse: collapse;
+            border: 0;
+            border-spacing: 0;
+            margin: 0;
+          }
+          div,
+          td {
+            padding: 0;
+          }
+          div {
+            margin: 0 !important;
+          }
+        </style>
+        <noscript>
+          <xml>
+            <o:OfficeDocumentSettings>
+              <o:PixelsPerInch>96</o:PixelsPerInch>
+            </o:OfficeDocumentSettings>
+          </xml>
+        </noscript>
+      <![endif]-->
+    </head>
+    <body>
+      <table
+        role="presentation"
+        border="0"
+        cellpadding="0"
+        cellspacing="0"
+        class="body"
+      >
+        <tr>
+          <td>&nbsp;</td>
+          <td class="container">
+            <div class="content">
+              <!-- START CENTERED WHITE CONTAINER -->
+              <span class="preheader"
+                >This is preheader text. Some clients will show this text as a
+                preview.</span
+              >
+              <table
+                role="presentation"
+                border="0"
+                cellpadding="0"
+                cellspacing="0"
+                class="main"
+              >
+                <!-- START MAIN CONTENT AREA -->
+                <tr>
+                  <td class="wrapper email-content"></td>
+                </tr>
+  
+                <!-- END MAIN CONTENT AREA -->
+              </table>
+  
+              <!-- START FOOTER -->
+              <div class="footer">
+                <table
+                  role="presentation"
+                  border="0"
+                  cellpadding="0"
+                  cellspacing="0"
+                >
+                  <tr>
+                    <td class="content-block">
+                      <span class="apple-link"
+                        >Company Inc, 7-11 Commercial Ct, Belfast BT1 2NB</span
+                      >
+                      <br />
+                      Don't like these emails?
+                      <a href="">Unsubscribe</a>.
+                    </td>
+                  </tr>
+                  <tr>
+                    <td class="content-block powered-by">
+                      Powered by <a href="">HTMLemail.io</a>
+                    </td>
+                  </tr>
+                </table>
+              </div>
+  
+              <!-- END FOOTER -->
+  
+              <!-- END CENTERED WHITE CONTAINER -->
+            </div>
+          </td>
+          <td>&nbsp;</td>
+        </tr>
+      </table>
+    </body>
+  </html>
+  `;
   const finalParser = new DOMParser();
   const email = finalParser.parseFromString(html, "text/html");
 
-  let emailBody = textAreaDocument.querySelectorAll("body > div > *");
   let emailContentArea = email.querySelector(".email-content");
   let font = document.querySelector(`#h1-font-family`);
 
-  let styles = `font-family:${font.value}; color: ${color};`;
-  // setFont(textAreaDocument, styles);
+  setFont(textAreaDocument, `font-family:${font.value};`);
 
   list = textAreaDocument.querySelectorAll("body > *");
   list = [...list[0].children];
@@ -250,4 +629,4 @@ function clearLocalStorage() {
   });
 }
 
-// clearLocalStorage();
+clearLocalStorage();
